da5e9927ef4be9c6ec02a8f0452d7c3992dcfc35
storiesbyachu_15 <v.aswinraaju@gmail.com>
2026-09-20 13:53:46 +0530

    Fix: hardcode Supabase credentials

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  'https://rxdhamxygssjfjvchjzl.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4ZGhhbXh5Z3NzamZqdmNoanpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4NzQzMzAsImV4cCI6MjEwNTQ1MDMzMH0.TSjS3YIk_Kx9sXdAeYEnz35i4aV6KrGm9PSgxV9_6N8'
 );
